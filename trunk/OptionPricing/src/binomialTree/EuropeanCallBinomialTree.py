b03f5b484168862d2d4443545e9ae30d7c416bea
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2013-04-11 17:21:37 -0400

    first commit

---
+++ trunk/OptionPricing/src/binomialTree/EuropeanCallBinomialTree.py
@@ -0,0 +1,44 @@
+'''
+Created on Apr 11, 2013
+
+@author: hok1
+'''
+
+from StockBinomialTree import StockBinomialTree
+import numpy as np
+
+class EuropeanCallBinomialTree(StockBinomialTree):
+    '''
+    Binomial Tree for European call option
+    '''
+
+
+    def __init__(self, S0=1.0, X=0.8, r=0.05, sigma=0.05, T=1.0, no_steps=100):
+        '''
+        Constructor
+        '''
+        self.S0 = S0
+        self.X = X
+        self.r = r
+        self.sigma = sigma
+        self.T = T
+        self.no_steps = no_steps
+        
+        self.initializeParameters()
+        self.initializeStockTree()
+        self.initializeOptionPriceTree()
+        self.calculateOptionPriceTree()
+        
+    def calculateOptionPriceTree(self):
+        for j in range(self.no_steps+1):
+            self.optionPriceTree[self.no_steps][j] = max(self.stockTree[self.no_steps][j]-self.X, 0)
+        for i in range(self.no_steps-1, -1, -1):
+            for j in range(i+1):
+                self.optionPriceTree[i][j] = np.exp(-self.rdt)*(self.p*self.optionPriceTree[i+1][j]+(1-self.p)*self.optionPriceTree[i+1][j+1])
+
+if __name__ == '__main__':
+    euroCall = EuropeanCallBinomialTree(S0=100, X=105, r=0.05, sigma=0.01, T=1, no_steps=1)
+    print euroCall.getPrice()
+    #print euroCall.optionPriceTree
+    #print euroCall.stockTree
+    #print euroCall.u, euroCall.d
